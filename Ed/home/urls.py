358171b021d8130986da72636403591a52e97f58
Edward <lkaudu@gmail.com>
2022-01-23 10:21:43 +0500

    Your comment

---
+++ Ed/home/urls.py
@@ -0,0 +1,43 @@
+from django.urls import path, include
+from django.http import HttpResponse
+
+from .views import (
+    home_page, 
+    successful_page,
+    register_page, 
+    # register2_page,
+    # register3_page,
+    login_page, 
+    login2_page, 
+    login3_page, 
+    logout_page,  
+    reset_view, 
+    passreset,
+
+
+    )
+
+# def okay (request):
+#     return HttpResponse('pretend-binary-data-here', content_type='image/jpeg')
+
+
+
+urlpatterns = [
+    # url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
+    # path('favicon.ico', okay),
+    path('', home_page, name='home'),
+    path('', successful_page, name='Lsuccessful'),
+    path('register/', register_page, name='register'),
+    # path('register2/', register2_page, name='register2'),
+    # path('register3/', register3_page, name='register3'),
+    path('login/', login_page, name='login'),
+    path('login2/', login2_page, name='login2'),
+    path('login3/', login3_page, name='login3'),
+    # path('login/<str:uid>', login_from_uid, name='login_uid'),
+    path('logout/', logout_page, name='logout'),
+    path('reset/', reset_view, name='reset'),
+    path('passreset/', passreset, name='passreset'),
+    # path('about/', about, name='about'),
+    # path('reset/<str:uid>', reset_from_uid, name='reset_uid'),
+    
+]
---
+++ Ed/home/views.py
@@ -0,0 +1,325 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from graphical_pwd_auth.settings import N, TBA, EMAIL_HOST_USER, ALLOWED_HOSTS
+from .models import LoginInfo
+import random, uuid
+
+
+def get_pwd_imgs():
+    # These images are just to confuse the attacker
+    images = random.sample(range(1, 13), N * 4)
+    print(images)
+    p_images = []
+    for i in range(0, N * 4, N):
+        p_images.append(images[i:i+N])
+    print(p_images)
+    return p_images
+
+def get_pwd_imgs2():
+    # These images are just to confuse the attacker
+    images = random.sample(range(1, 13), N * 4)
+    print(images)
+    p_images = []
+    for i in range(0, N * 4, N):
+        p_images.append(images[i:i+N])
+    print(p_images)
+    return p_images
+
+
+
+def get_clpwd():
+    
+    images = random.sample(range(1, 10), N * 3)
+    print(images)
+    p_colours = []
+    for i in range(0, N * 3, N):
+        p_colours.append(images[i:i+N])
+    print(p_colours)
+    return p_colours
+    
+
+
+def update_login_info(user, didSuccess):
+    if didSuccess:
+        user.logininfo.fails = 0
+    else:
+        user.logininfo.fails += 1
+    
+    user.logininfo.save()
+    print('{} Failed attempts: {}'.format(user.username, user.logininfo.fails))
+
+
+
+def isBlocked(username):
+    try:
+        user = User.objects.get(username=username)
+    except Exception:
+        return None
+    print('isBlocked: {} - {}'.format(user.logininfo, TBA))
+    if user.logininfo.fails >= TBA:
+        return True
+    else:
+        return False
+
+
+def sendLoginLinkMailToUser(username):
+    user = User.objects.get(username=username)
+    # send email only id user.logininfo.login_link is not None
+    if user.logininfo.login_link is None:
+        link = str(uuid.uuid4())
+        user.logininfo.login_link = link
+        user.logininfo.save()
+        email = EmailMessage(
+            subject='Link to Log in to your account',
+            body='''
+            Someone tried to bruteforce on your account.
+            Click the Link to Login to your account directly.
+            The link is one-time clickable
+            link: http://{}:8000/login/{}
+            '''.format(ALLOWED_HOSTS[-1], link), # might wanna change the allowd_host
+            from_email=EMAIL_HOST_USER,
+            to=[user.email],
+        )
+        email.send()
+        print('LOGIN LINK EMAIL SENT')
+
+
+def sendPasswordResetLinkToUser(username):
+    # send reset link everytime user requests
+    try:
+        user = User.objects.get(username=username)
+        return True
+
+    except Exception:
+        return False
+    
+    # link = str(uuid.uuid4())
+    # user.logininfo.reset_link = link
+    # user.logininfo.save()
+    # email = EmailMessage(
+    #     subject='Link to Rest your Password',
+    #     body='''
+    #     You have requested to reset your password.
+    #     Click the Link to reset your password directly.
+    #     The link is one-time clickable
+    #     link: http://{}:8000/reset/{}
+    #     '''.format(ALLOWED_HOSTS[-1], link), # might wanna change the allowd_host
+    #     from_email=EMAIL_HOST_USER,
+    #     to=[user.email],
+    
+    # print('PWD RESET LINK EMAIL SENT')
+    
+
+
+def home_page(request):
+    return render(request, 'home.html')
+
+
+def successful_page(request):
+    return render(request, 'successful.html')
+
+
+def register_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        print(username, password)
+        try:
+            # create user and loginInfo for him
+            user = User.objects.create_user(email=email, username=username, password=password, first_name=first_name, last_name=last_name)
+            login_info = LoginInfo(user=user, fails=0)
+            login_info.save()
+            messages.success(request, 'Sign up successful')
+        except Exception:
+            messages.warning(request, 'Sign up failed. Kindly pick another username')
+            return redirect('register')
+        # return redirect('home')
+    else:
+        data = {
+            'p_images': get_pwd_imgs(),
+        }
+        return render(request, 'register.html', context=data)
+    return redirect('home')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # first_name = request.POST['first_name']
+        # last_name = request.POST['last_name']
+        print(username, password)
+
+        block_status = isBlocked(username)
+        if block_status is None:
+            # No user exists
+            messages.warning(request, 'Account doesn\'t Exist')
+            return redirect('login')
+
+        elif block_status == True:
+            # Blocked - send login link to email
+            # check if previously sent, if not send
+            sendLoginLinkMailToUser(username)
+            messages.warning(request, 'Your account is Blocked, please check your Email!')
+            return redirect('login')
+        else:
+            # Not Blocked
+            usr = authenticate(username=username, password=password, request=request)
+            if usr is not None:
+                # login(request, usr)
+                # update_login_info(usr, True)
+                messages.success(request, 'Good, kindly proceed to the 2nd Login Phase')
+                return redirect ('login2')
+                
+            
+            else:
+                user = User.objects.get(username=username)
+                update_login_info(user, False)
+                messages.warning(request, 'Sorry, You entered a wrong Password! Try again')
+                return redirect('login')
+
+    else:
+        data = {
+            'p_images': get_pwd_imgs(),
+        }
+        return render(request, 'login.html', context=data)
+    return redirect('login2')
+
+def login2_page(request):
+    if request.method == 'POST':
+                
+        first_name = request.POST['first_name']
+        # user = User.objects.get(username=username)
+        user = User.objects.filter(first_name=first_name).first()
+        if not user:
+            messages.warning(request, 'Sorry, You entered a wrong Password! Try again')
+            return redirect ('login2')
+                    
+        else:
+            messages.warning(request, 'Good! Kindly proceed to the Final Login Phase')
+            return redirect ('login3')
+            
+    else:
+        data = {
+        'p_images': get_pwd_imgs(),
+    }
+        return render(request, 'login2.html', context=data)
+    return redirect('login3')
+
+def login3_page(request):
+    if request.method == 'POST':
+                
+        last_name = request.POST['last_name']
+        # user = User.objects.get(username=username)
+        user = User.objects.filter(last_name=last_name).first()
+        if not user:
+            messages.warning(request, 'Sorry, You entered a wrong Password! Try again')
+            return redirect ('login3')
+                    
+        else:
+            messages.warning(request, 'Login Successful!')
+            login(request, user)
+            update_login_info(user, True)
+            
+            return redirect ('home')
+            
+    else:
+        data = {
+        'p_images': get_pwd_imgs(),
+    }
+        return render(request, 'login3.html', context=data)
+    return redirect('login3')
+    
+
+def login_from_uid(request, uid):
+    try:
+        # get user from the uid and reset the Link to 'NO_LINK' again
+        login_info = LoginInfo.objects.get(login_link=uid)
+        user = login_info.user
+        login(request, user)
+        update_login_info(user, True)
+        login_info.login_link = None
+        login_info.save()
+        messages.success(request, 'Login successfull!')
+    except Exception:
+        messages.warning(request, 'Invalid Link. Please check again!')
+
+    return redirect('home')
+
+
+def reset_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        user = User.objects.filter(username=username).first()
+        if not user:
+            messages.warning(request, 'Username Name Not found!')
+            return render(request, 'reset_request.html')
+                    
+        else:
+            messages.warning(request, 'Reset Your Password here!')
+            return redirect ('passreset')
+            
+
+    else:
+        return render(request, 'reset_request.html')
+
+def passreset(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        # email = request.POST['email']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        print(username, password)
+        
+        # login_info = LoginInfo.objects.get(reset_link=uid)
+        user = User.objects.get(username=username)
+        # user = login_info.user
+        if not user:
+            # reset user and loginInfo for him
+            messages.warning(request, 'Please check again!')
+            return redirect('passreset')
+            
+
+        else:
+           
+            # # reset pwd
+            # user.set_password = User.cleaned_data['password']
+            # user.set_first_name = User.cleaned_data['first_name']
+            # user.set_last_name = User.cleaned_data['last_name']
+            user.set_password(password)
+            user.set_password(first_name)
+            user.set_password(last_name)
+
+            # user.password = User.cleaned_data['password']
+            # user.first_name = User.cleaned_data['first_name']
+            # user.last_name = User.cleaned_data['last_name']
+            
+           
+            # login_info.save()
+            user.save()
+            messages.success(request, 'Password Changed Successfully!')
+            return redirect('home')
+
+            
+           
+    else:
+        data = {
+            'p_images': get_pwd_imgs(),
+        }
+        return render(request, 'reset.html', context=data)
+    return redirect('home')
+
+
+
+
+def logout_page(request):
+    logout(request)
+    messages.warning(request, 'You\'ve been logged out!')
+    return redirect('home')
